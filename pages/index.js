a611768a97ed7a326ecd00f626271e5d4ac9796f
akhil chaudhary <chaudharyakhil337@gmail.com>
2021-10-17 00:05:18 +0530

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,16 @@
 import Head from "next/head";
+import Feed from "../components/Feed";
 import Header from "../components/Header";
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="bg-gray-50 h-screen overflow-y-scroll scrollbar-hide">
       <Head>
         <title>INSTAGRAM CLONE</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {/* header */}
       <Header />
-      {/* feed */}
+      <Feed />
       {/* Modal */}
     </div>
   );
