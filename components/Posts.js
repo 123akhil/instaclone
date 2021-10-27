bff2b634237298f94b609cc12d7a18f648823aa1
akhil chaudhary <chaudharyakhil337@gmail.com>
2021-10-27 15:14:46 +0530

    commit

--- components/Posts.js
+++ components/Posts.js
@@ -1,31 +1,30 @@
+import { collection, onSnapshot, orderBy, query } from "@firebase/firestore";
+import { useEffect, useState } from "react";
+import { db } from "../firebase";
 import Post from "./Post";
-const DUMMY_DATA = [
-  {
-    id: "123",
-    username: "akhil chaudhary",
-    userImg: "https://links.papareact.com/3ke",
-    img: "https://links.papareact.com/3ke",
-    caption: "Now this is how we roll ...<<BABY>>...",
-  },
-  {
-    id: "123",
-    username: "akhil chaudhary",
-    userImg: "https://links.papareact.com/3ke",
-    img: "https://links.papareact.com/3ke",
-    caption: "Now this is how we roll ...<<BABY>>...",
-  },
-];
 function Posts() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(
+    () =>
+      onSnapshot(
+        query(collection(db, "posts"), orderBy("timestamp", "desc")),
+        (snapshot) => {
+          setPosts(snapshot.docs);
+        }
+      ),
+    [db]
+  );
   return (
     <div>
-      {DUMMY_DATA.map((post) => (
+      {posts.map((post) => (
         <Post
           key={post.id}
           id={post.id}
-          username={post.username}
-          userImg={post.userImg}
-          img={post.img}
-          caption={post.caption}
+          username={post.data().username}
+          userImg={post.data().profileImg}
+          img={post.data().image}
+          caption={post.data().caption}
         />
       ))}
     </div>
